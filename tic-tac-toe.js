b4928fa785436a5299c2f10aeef7eebdd693132e
João Victor de Souza <j.victor@sinova.com.br>
2022-05-02 01:09:58 -0300

    [Update]: atualizado a função 'SelectSquare' em tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,21 @@
 var player = null;
 var selectedPlayer = document.getElementById('selectedPlayer');
+var squares = document.getElementsByClassName('square');
 
 ChangePlayer('X');
 
 function SelectSquare(id){
     var square = document.getElementById(id);
+
+    if (square.innerHTML !== '-') return;
+
     square.innerHTML = player;
     square.style.color = '#000';
+
+    if (player === 'X') player = 'O';
+    else player = 'X';
+
+    ChangePlayer(player);
 }
 
 function ChangePlayer(value){
